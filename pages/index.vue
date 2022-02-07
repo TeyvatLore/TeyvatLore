66f463423172d47068ff45d2cce739871042621d
代小呆 <diauweb@live.com>
2022-02-07 04:27:40 +0000

    chore: lint

--- pages/index.vue
+++ pages/index.vue
@@ -33,35 +33,35 @@
         <l-control class="nav-wrapper" position="topleft">
           <div id="side-nav">
             <div id="location-btn">
-              <button v-on:click="selectRegion">
+              <button @click="selectRegion">
                 <img
                   name="0"
                   :src="imgFolder + regions[0] + regionImgPostfix[0] + '.png'"
-                />
+                >
               </button>
-              <button v-on:click="selectRegion">
+              <button @click="selectRegion">
                 <img
                   name="1"
                   :src="imgFolder + regions[1] + regionImgPostfix[1] + '.png'"
-                />
+                >
               </button>
-              <button v-on:click="selectRegion">
+              <button @click="selectRegion">
                 <img
                   name="2"
                   :src="imgFolder + regions[2] + regionImgPostfix[2] + '.png'"
-                />
+                >
               </button>
-              <button v-on:click="selectRegion">
+              <button @click="selectRegion">
                 <img
                   name="3"
                   :src="imgFolder + regions[3] + regionImgPostfix[3] + '.png'"
-                />
+                >
               </button>
-              <button v-on:click="selectRegion">
+              <button @click="selectRegion">
                 <img
                   name="4"
                   :src="imgFolder + regions[4] + regionImgPostfix[4] + '.png'"
-                />
+                >
               </button>
             </div>
           </div>
@@ -78,21 +78,21 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import "@/assets/styles/global.css";
+import Vue from 'vue'
+import '@/assets/styles/global.css'
 
-const mapCenter = [3568, 6286];
-const mapSize = [12288, 15360];
+const mapCenter = [3568, 6286]
+const mapSize = [12288, 15360]
 
 export default Vue.extend({
-  name: "IndexPage",
-  async asyncData({ $content }) {
-    const markers = await $content("markers").fetch();
+  name: 'IndexPage',
+  async asyncData ({ $content }) {
+    const markers = await $content('markers').fetch()
     return {
-      markers: (markers as any).markers,
-    };
+      markers: (markers as any).markers
+    }
   },
-  data() {
+  data () {
     return {
       maxBounds: null as unknown as L.LatLngBounds,
       mapCRS: null as unknown as L.CRS,
@@ -100,34 +100,20 @@ export default Vue.extend({
       mapReady: false,
       posX: 0,
       posY: 0,
-      currentRegion: "蒙德",
-      regions: ["蒙德", "璃月", "稻妻", "龙脊雪山", "渊下宫"],
-      regionImgPostfix: ["1", "", "", "", ""],
-      imgFolder: "./images/",
-    };
+      currentRegion: '蒙德',
+      regions: ['蒙德', '璃月', '稻妻', '龙脊雪山', '渊下宫'],
+      regionImgPostfix: ['1', '', '', '', ''],
+      imgFolder: './images/'
+    }
   },
-  methods: {
-    // switch region on map
-    selectRegion: function (event: any) {
-      let index = Number(event.target.name);
-      this.currentRegion = this.regions[index];
-      for (let i = 0; i < this.regionImgPostfix.length; i++) {
-        if (index === i) {
-          this.regionImgPostfix[i] = "1";
-        } else {
-          this.regionImgPostfix[i] = "";
-        }
-      }
-    },
-  },
-  mounted() {
+  mounted () {
     this.maxBounds = this.$L.latLngBounds(
       this.$L.latLng(-mapCenter[0] - 10000, -mapCenter[1] - 10000),
       this.$L.latLng(
         mapSize[0] - mapCenter[0] + 10000,
         mapSize[1] - mapCenter[1] + 10000
       )
-    );
+    )
 
     this.mapCRS = this.$L.Util.extend({}, this.$L.CRS.Simple, {
       transformation: new this.$L.Transformation(1, 0, 1, 0),
@@ -136,31 +122,31 @@ export default Vue.extend({
           return new this.$L.Point(
             latlng.lat + mapCenter[0],
             latlng.lng + mapCenter[1]
-          );
+          )
         },
         unproject: (point: L.Coords) => {
           return new this.$L.LatLng(
             point.x - mapCenter[0],
             point.y - mapCenter[1]
-          );
-        },
+          )
+        }
       },
       bounds: this.$L.bounds(
         this.$L.point(0, 0),
         this.$L.point(mapSize[0], mapSize[1])
-      ),
-    });
+      )
+    })
 
     const TileLayerClass = this.$L.TileLayer.extend({
-      getTileUrl(coords: L.Coords) {
-        const [x, y, z] = [coords.x, coords.y, coords.z + 13];
-        return `https://assets.yuanshen.site/tiles_twt/${z}/${x}_${y}.png`;
+      getTileUrl (coords: L.Coords) {
+        const [x, y, z] = [coords.x, coords.y, coords.z + 13]
+        return `https://assets.yuanshen.site/tiles_twt/${z}/${x}_${y}.png`
       },
-      reuseTiles: true,
-    });
+      reuseTiles: true
+    })
 
     // @ts-ignore
-    this.tileLayer = new TileLayerClass("", {
+    this.tileLayer = new TileLayerClass('', {
       maxZoom: 10,
       minZoom: -6,
       maxNativeZoom: 0,
@@ -168,25 +154,39 @@ export default Vue.extend({
       bounds: this.$L.latLngBounds(
         this.$L.latLng(-mapCenter[0], -mapCenter[1]),
         this.$L.latLng(mapSize[0] - mapCenter[0], mapSize[1] - mapCenter[1])
-      ),
-    });
+      )
+    })
 
-    this.mapReady = true;
+    this.mapReady = true
     this.$nextTick(function () {
-      const mapObject = (this.$refs.mapi as any).mapObject;
-      mapObject.addLayer(this.tileLayer);
+      const mapObject = (this.$refs.mapi as any).mapObject
+      mapObject.addLayer(this.tileLayer)
       for (const m of (this as any).markers) {
         this.$L
           .marker((m as any).pos, { title: (m as any).name })
-          .addTo(mapObject);
+          .addTo(mapObject)
       }
-      mapObject.on("mousemove", (e: L.LeafletMouseEvent) => {
-        this.posX = e.latlng.lat;
-        this.posY = e.latlng.lng;
-      });
-    });
+      mapObject.on('mousemove', (e: L.LeafletMouseEvent) => {
+        this.posX = e.latlng.lat
+        this.posY = e.latlng.lng
+      })
+    })
   },
-});
+  methods: {
+    // switch region on map
+    selectRegion (event: any) {
+      const index = Number(event.target.name)
+      this.currentRegion = this.regions[index]
+      for (let i = 0; i < this.regionImgPostfix.length; i++) {
+        if (index === i) {
+          this.regionImgPostfix[i] = '1'
+        } else {
+          this.regionImgPostfix[i] = ''
+        }
+      }
+    }
+  }
+})
 </script>
 
 <style scoped>
